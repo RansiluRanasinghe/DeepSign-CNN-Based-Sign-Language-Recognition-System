02dffe9bc24eec21bd182041acaaa33e8cfe1944
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2025-12-27 13:14:49 +0530

    Implemeted the image preprocessing function

--- API/app/main.py
+++ API/app/main.py
@@ -25,4 +25,17 @@ app = FastAPI(
     description="Production ready API for recognizing Hand Sign",
     version="1.0.0",
     lifespan=lifespan
-)           
+)  
+
+def image_preprocessor(image_bytes: bytes):
+    try:
+        img = Image.open(io.BytesIO(image_bytes))
+        img = ImageOps.grayscale(img)
+        img = img.resize((28, 28))
+        img_array = np.array(img) / 255.0
+        img_array = img_array.reshape(1, 28, 28, 1)
+
+        return img_array.astype(np.float32)
+    except Exception as e:
+        raise ValueError("Failed to Preprocess: ", str(e))
+    
